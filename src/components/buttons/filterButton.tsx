3dc8c729a81c2b42cae9e5228a8be1f625115998
SAURETMathieu <mathieu.sauret@oclock.school>
2024-05-22 16:00:02 +0200

    feat(WalletTable): component for Wallet table values

---
+++ src/components/buttons/filterButton.tsx
@@ -0,0 +1,32 @@
+import { Button } from "@/src/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/src/components/ui/dropdown-menu";
+import { ListFilter } from "lucide-react";
+
+export default function FilterButton() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" size="sm" className="h-8 gap-1">
+          <ListFilter className="size-3.5" />
+          <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
+            Filter
+          </span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuLabel>Filter by</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuCheckboxItem checked>Active</DropdownMenuCheckboxItem>
+        <DropdownMenuCheckboxItem>Draft</DropdownMenuCheckboxItem>
+        <DropdownMenuCheckboxItem>Archived</DropdownMenuCheckboxItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
---
+++ src/components/buttons/tableActions.tsx
@@ -0,0 +1,28 @@
+import { MoreHorizontal } from "lucide-react";
+
+import { Button } from "../ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
+
+export default function TableActions() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button aria-haspopup="true" size="icon" variant="ghost">
+          <MoreHorizontal className="size-4" />
+          <span className="sr-only">Toggle menu</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuLabel>Actions</DropdownMenuLabel>
+        <DropdownMenuItem>Edit</DropdownMenuItem>
+        <DropdownMenuItem>Delete</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
