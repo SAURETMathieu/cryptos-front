88c502959827a1858af19488b39ac27d592eb8a4
SAURETMathieu <mathieu.sauret@oclock.school>
2024-05-22 16:02:16 +0200

    feat(page): add wallet page

--- src/app/[locale]/wallets/page.tsx
+++ src/app/[locale]/wallets/page.tsx
@@ -1,9 +1,22 @@
 "use client";
 
+import CardsSection from "@/src/components/sections/CardsSection";
+import WalletNav from "@/src/components/sections/WalletNav";
+import WalletSpread from "@/src/components/sections/WalletSpread";
+import WalletTable from "@/src/components/sections/WalletTable";
+
 export default function WalletsPage() {
   return (
-    <section className="container mx-0 grid items-center gap-6 pb-8 pt-6">
-      Wallets
-    </section>
+    <main className="flex min-h-screen flex-1 max-lg:flex-col sm:py-4 sm:pl-14">
+      <section className="flex w-full flex-col gap-2 lg:w-3/4">
+        <WalletNav />
+        <CardsSection />
+        <WalletTable />
+      </section>
+      <section className="w-full lg:min-h-screen lg:w-1/4">
+        <WalletSpread />
+        <WalletSpread />
+      </section>
+    </main>
   );
 }
