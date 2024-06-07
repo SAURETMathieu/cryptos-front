019941ec156675d5d0b4454e50161998dcb5c3ff
SAURETMathieu <mathieu.sauret.dev@gmail.com>
2024-06-07 20:59:34 +0200

    feat(WalletPage): create server component to render the page

--- src/app/[locale]/wallets/page.tsx
+++ src/app/[locale]/wallets/page.tsx
@@ -1,21 +1,5 @@
-import CardsSection from "@/src/components/sections/CardsSection";
-import WalletNav from "@/src/components/sections/WalletNav";
-import WalletSpread from "@/src/components/sections/WalletSpread";
-import WalletTableSection from "@/src/components/sections/WalletTableSection";
+import WalletPage from "./WalletPage";
 
-export default async function WalletsPage() {
-
-  return (
-    <main className="flex min-h-screen flex-1 max-lg:flex-col sm:py-4 sm:pl-14">
-      <section className="flex w-full flex-col gap-2 lg:w-8/12 xl:w-9/12 2xl:w-10/12">
-        <WalletNav />
-        <CardsSection />
-        <WalletTableSection />
-      </section>
-      <section className="w-full lg:min-h-screen lg:w-4/12 xl:w-3/12 2xl:w-2/12">
-        <WalletSpread />
-        <WalletSpread />
-      </section>
-    </main>
-  );
+export default async function Page() {
+  return <WalletPage/>;
 }
