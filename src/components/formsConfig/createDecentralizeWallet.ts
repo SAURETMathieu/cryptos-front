211784cbf29f55e655f59ba7c609564302e13b21
SAURETMathieu <mathieu.sauret.dev@gmail.com>
2024-06-07 21:00:29 +0200

    fix(wallet schema): fix type of values

--- src/components/formsConfig/createDecentralizeWallet.ts
+++ src/components/formsConfig/createDecentralizeWallet.ts
@@ -1,39 +1,51 @@
+import fetchApi from "@/services/api/fetchApi";
+import { getRegexForBlockchain } from "@/src/data/addressesRegex";
 import { blockchains } from "@/src/data/tableLabels";
 import { z } from "zod";
-
-import fetchApi from "@/services/api/fetchApi";
 
 const blockchainValues = blockchains
   .filter((blockchain) => blockchain.value !== "All")
   .map((blockchain) => blockchain.value) as [string, ...string[]];
 
 export const generateDecentralizeFormSchema = (datas: any = {}) => {
-  return z.object({
-    name: z
-      .string({
-        required_error: "Wallet's name is required.",
-      })
-      .max(30, "Wallet must be at most 30 characters.")
-      .min(3, "Wallet must be at least 3 characters.")
-      .default(datas.name ?? undefined),
+  return z
+    .object({
+      name: z
+        .string({
+          required_error: "Wallet's name is required.",
+        })
+        .max(30, "Wallet must be at most 30 characters.")
+        .min(3, "Wallet must be at least 3 characters.")
+        .default(datas.name ?? undefined),
 
-    blockchain: z
-      .enum(blockchainValues, {
-        required_error: "Blockchain is required.",
-      })
-      .refine((val) => val !== undefined, {
-        message: "Blockchain is required.",
-      })
-      .default(datas.blockchain ?? undefined),
+      blockchain: z
+        .enum(blockchainValues, {
+          required_error: "Blockchain is required.",
+        })
+        .refine((val) => val !== undefined, {
+          message: "Blockchain is required.",
+        })
+        .default(datas.blockchain ?? undefined),
 
-    address: z
-      .string({
-        required_error: "Wallet's address is required.",
-      })
-      .max(30, "Wallet must be at most 30 characters.")
-      .min(3, "Wallet must be at least 3 characters.")
-      .default(datas.address ?? undefined),
-  });
+      address: z
+        .string({
+          required_error: "Wallet's address is required.",
+        })
+        .max(44, "Wallet must be at most 44 characters.")
+        .min(26, "Wallet must be at least 26 characters.")
+        .default(datas.address ?? undefined),
+    })
+    .refine(
+      (data) => {
+        const regex = getRegexForBlockchain(data.blockchain);
+        if (!regex) return false;
+        return regex.test(data.address);
+      },
+      {
+        message: `This address is unvalid for this blockchain.`,
+        path: ["address"],
+      }
+    );
 };
 
 export const decentralizeFormSchema = generateDecentralizeFormSchema();
@@ -53,6 +65,7 @@ export const fieldConfig = {
   },
   address: {
     label: "Address",
+    description: "Your adress with 0x prefix.",
     inputProps: {
       placeholder: "Enter your wallet address",
     },
@@ -64,14 +77,20 @@ export const onSubmit = async (
   closeSheet?: () => void
 ) => {
   try {
-    const isSuccess = await fetchApi("POST", "wallets/decentralized", values);
-    if (!isSuccess) {
+    const newWallet = await fetchApi(
+      "POST",
+      "wallets/decentralized",
+      values,
+      true
+    );
+    if (!newWallet) {
       throw new Error("Failed to create wallet");
     }
     closeSheet?.();
-    console.log(isSuccess);
+    return newWallet;
   } catch (error) {
     console.error(error);
+    return null;
   }
 };
 
