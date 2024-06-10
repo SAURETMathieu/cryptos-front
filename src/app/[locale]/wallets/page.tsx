fa565aad014db9eb8b50a54b6314e9a6faa92f8f
SAURETMathieu <mathieu.sauret.dev@gmail.com>
2024-06-10 21:16:22 +0200

    feat(wallet context): apply provider to wallets page

--- src/app/[locale]/wallets/page.tsx
+++ src/app/[locale]/wallets/page.tsx
@@ -1,5 +1,27 @@
+import { redirect } from "next/navigation";
+import fetchApi from "@/services/api/fetchApi";
+import { WalletProvider } from "@/src/context/walletsProvider";
+
+import { auth } from "@/lib/auth";
+
 import WalletPage from "./WalletPage";
 
 export default async function Page() {
-  return <WalletPage/>;
+  const session = await auth();
+  let wallets: any[] = [];
+  if (session) {
+    const fetchedWallets = await fetchApi<any[] | null>("GET", "wallets", null, session.account.id_token);
+    if (fetchedWallets && Array.isArray(fetchedWallets)) {
+      wallets = fetchedWallets;
+    }
+  } else {
+    //TODO récupérer des données fakes
+    redirect("/login");
+  }
+
+  return (
+    <WalletProvider initialWallets={wallets}>
+      <WalletPage />
+    </WalletProvider>
+  );
 }
