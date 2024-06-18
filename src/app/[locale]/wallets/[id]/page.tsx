77a30211ef0d5867f7f75547800eb4c04a920346
SAURETMathieu <mathieu.sauret.dev@gmail.com>
2024-06-18 19:39:11 +0200

    feat(wallet): fetch wallet with cryptos

--- src/app/[locale]/wallets/[id]/page.tsx
+++ src/app/[locale]/wallets/[id]/page.tsx
@@ -1,22 +1,22 @@
-import CardsSection from "@/src/components/sections/CardsSection";
-import CryptosTableSection from "@/src/components/sections/CryptosTableSection";
-import WalletNav from "@/src/components/sections/WalletNav";
-import WalletSpread from "@/src/components/sections/WalletSpread";
-import InfosBlock from "@/src/components/sections/InfosBlock";
+import { redirect } from "next/navigation";
+import fetchApi from "@/services/api/fetchApi";
 
-export default function WalletPage({ params }: { params: { id: number } }) {
+import { auth } from "@/lib/auth";
+
+import WalletPage from "./WalletPage";
+
+export default async function Page({ params }: { params: { id: number } }) {
+  const session = await auth();
+  let walletWithCryptos = {} as any;
+  if (session) {
+    const fetchedWallet = await fetchApi("GET", `wallets/${params.id}`, null, session.account.id_token);
+    walletWithCryptos = fetchedWallet;
+  } else {
+    //TODO récupérer des données fakes
+    redirect("/login");
+  }
+
   return (
-    <main className="flex min-h-screen flex-1 max-lg:flex-col sm:py-4 sm:pl-14">
-      <section className="flex w-full flex-col gap-2 lg:w-8/12 xl:w-9/12 2xl:w-10/12">
-        <WalletNav />
-        <CardsSection />
-        <InfosBlock/>
-        <CryptosTableSection />
-      </section>
-      <section className="w-full lg:min-h-screen lg:w-4/12 xl:w-3/12 2xl:w-2/12">
-        <WalletSpread />
-        <WalletSpread />
-      </section>
-    </main>
+    <WalletPage walletWithCryptos={walletWithCryptos}/>
   );
 }
