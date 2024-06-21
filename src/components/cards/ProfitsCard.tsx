65719de98eb7c71d70663685f6ab6c66d3a42629
SAURETMathieu <mathieu.sauret.dev@gmail.com>
2024-06-21 15:19:27 +0200

    feat(ProfitsCard): dynamize profit value

--- src/components/cards/ProfitsCard.tsx
+++ src/components/cards/ProfitsCard.tsx
@@ -1,13 +1,27 @@
+"use client";
+
+import { useLocale } from "next-intl";
+
+import useStore from "@/hooks/useStore";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 export default function ProfitsCard() {
+  const locale = useLocale();
+  const profitsTotal: any = useStore((state) => state.profitsTotal);
+  const options = {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  };
+  const totalProfitsFormatted = new Intl.NumberFormat(locale, options).format(
+    profitsTotal
+  );
   return (
     <Card className="">
       <CardHeader className="">
         <CardTitle className="text-xl font-medium">Profits</CardTitle>
       </CardHeader>
       <CardContent>
-        <div className="text-2xl font-bold">$45,231.89</div>
+        <div className="text-2xl font-bold">$ {totalProfitsFormatted}</div>
         <p className="text-xs text-muted-foreground">+20.1% from last month</p>
       </CardContent>
     </Card>
