6f1466f23ef9d2183359bba4010fe698dde4c8c7
SAURETMathieu <mathieu.sauret.dev@gmail.com>
2024-06-21 15:19:45 +0200

    feat(WalletSpread): component to get wallets balances graph

--- src/components/sections/WalletSpread.tsx
+++ src/components/sections/WalletSpread.tsx
@@ -1,4 +1,6 @@
-import PieChart from "@/src/components/charts/PieChart";
+"use client";
+
+import { PieChartGraph } from "@/src/components/charts/PieChart";
 import {
   Card,
   CardContent,
@@ -8,7 +10,20 @@ import {
   CardTitle,
 } from "@/src/components/ui/card";
 
+import useStore from "@/hooks/useStore";
+
 export default function WalletSpread() {
+  const wallets: any = useStore((state) => state.wallets);
+  const datas = wallets.map((wallet: any) => ({
+    name: wallet.name,
+    value: parseFloat(wallet.balance.toFixed(2)),
+  }));
+  datas.sort((a: any, b: any) => b.value - a.value);
+  if(datas.length > 5) {
+    const others = datas.slice(4);
+    const othersValue = others.reduce((acc: number, cur: any) => acc + cur.value, 0);
+    datas.splice(4, datas.length - 4, { name: "Others", value: parseFloat(othersValue.toFixed(2)) });
+  }
   return (
     <section className="w-full p-4 pt-0 lg:pl-0">
       <Card className="h-fit w-full max-w-full">
@@ -17,16 +32,15 @@ export default function WalletSpread() {
             Spread of Wallets
           </CardTitle>
           <CardDescription>
-            Spread of wallets across different cryptos or wallets
+            Spread of your wallets balances.
           </CardDescription>
         </CardHeader>
         <CardContent className="w-full p-2">
-          <PieChart />
+          <PieChartGraph datas={datas} />
         </CardContent>
         <CardFooter>
           <p className="text-sm text-muted-foreground">
-            This section will show the spread of wallets across different
-            cryptos or wallets
+            This section shows the spread of your wallets balances in percent.
           </p>
         </CardFooter>
       </Card>
