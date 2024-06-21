471f33a69816e89faf9c5f533a9a769f6fd5b06c
SAURETMathieu <mathieu.sauret.dev@gmail.com>
2024-06-21 15:19:06 +0200

    feat(utils): function aggregateBalances to get all cryptos balances

---
+++ src/utils/aggregateBalances.ts
@@ -0,0 +1,29 @@
+export const aggregateBalances = (wallets:any[]) => {
+  const balanceMap = new Map();
+
+  wallets.forEach((wallet: any) => {
+    wallet.balances.forEach((balance: any) => {
+      const { asset, nbToken, price } = balance;
+      if (balanceMap.has(asset)) {
+        balanceMap.set(asset, balanceMap.get(asset) + nbToken * price);
+      } else {
+        balanceMap.set(asset, nbToken * price);
+      }
+    });
+  });
+
+  const datas = Array.from(balanceMap).map(([asset, value]) => ({
+    name: asset,
+    value: parseFloat(value.toFixed(2)),
+  }));
+
+  datas.sort((a, b) => b.value - a.value);
+
+  if (datas.length > 5) {
+    const others = datas.slice(4);
+    const othersValue = others.reduce((acc, cur) => acc + cur.value, 0);
+    datas.splice(4, datas.length - 4, { name: "Others", value: parseFloat(othersValue.toFixed(2)) });
+  }
+
+  return datas;
+};
