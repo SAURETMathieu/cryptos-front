ec0ed636e46417ccd37e41f04bb8453b6f3faf3c
SAURETMathieu <mathieu.sauret.dev@gmail.com>
2024-07-03 13:29:05 +0200

    feat(utils): function to formatDate per language

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,8 +1,8 @@
-export default function formatDate(timestamp: string, language: string) {
+export default function formatDate(timestamp: string, language: string = "fr") {
   const options: Intl.DateTimeFormatOptions = {
-    day: '2-digit',
-    month: '2-digit',
-    year: '2-digit'
+    day: "2-digit",
+    month: "2-digit",
+    year: "2-digit",
   };
   return new Date(timestamp).toLocaleDateString(language, options);
 }
